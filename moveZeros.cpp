1d99de0c3168b37410177204898dd4fec103f7fc
dev kushwaha <dev.kushwaha.7983@gmail.com>
2023-10-14 15:05:14 +0530

    moveZeros.cpp

---
+++ moveZeros.cpp
@@ -0,0 +1,20 @@
+// 283
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                for(int j=i+1;j<nums.size();j++){
+                    if(nums[j]!=0){
+                        nums[i]=nums[j];
+                        nums[j]=0;
+                break;
+                    }
+
+                }
+            }
+        }
+
+        
+    }
+};
