ee64d9f934d6ef51304f5746ec3c271eb26e70f8
dev kushwaha <dev.kushwaha.7983@gmail.com>
2024-03-28 01:46:39 +0530

    frequencyOfTheMostFrequentElement

---
+++ frequencyOfTheMostFrequentElement.cpp
@@ -0,0 +1,26 @@
+// 1838. Frequency of the Most Frequent Element
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+            long long  l=0,r=0;
+            long long ans =0 , total =0;
+
+
+            while(r<nums.size()){
+                total+=nums[r];
+                while( nums[r]*(r-l+1)>total+k){
+                    total -=nums[l];
+                    l++;
+                }
+                ans = max(ans,(r-l+1));
+                r++;
+
+            }
+            return ans;
+
+        
+        
+    }
+};
